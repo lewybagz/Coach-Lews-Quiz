337b47d9ddcf233d52440646cdd5e651d599ad21
Lewis Patterson <lleep1997@gmail.com>
2023-09-11 17:25:07 -0700

    Remove unnecessary files

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/Coach-Lews-Quiz/",
-  plugins: [react()],
-});
